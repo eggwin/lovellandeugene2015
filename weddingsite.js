bedbd8473c2af53da082491e3b1235817f215a38
eggwin <ehourany@gmail.com>
2015-04-09 22:59:31 -0700

    Initial Commit

---
+++ weddingsite.js
@@ -0,0 +1,270 @@
+if (Meteor.isClient) {
+
+  Template.lodging.events({
+    "click #reserveRoom": function (e, template) {
+      // opens in a new tab
+      var url = 'http://www.marriott.com/meeting-event-hotels/group-corporate-travel/groupCorp.mi?resLinkData=Hourany-Abungan%20Wedding%5Esfosb%60HAWHAWA%60149.00%60USD%60false%6010/16/15%6010/18/15%609/18/15&app=resvlink&stop_mobi=yes';
+      window.open(url, '_blank');
+    }
+  });
+
+  Template.contactus.events({
+    "submit form": function (e, template) {
+      e.preventDefault();
+      e.stopPropagation();
+      var fullname = template.$('#fullname').val(),
+          email = template.$('#emailaddress').val(),
+          message = template.$('#message').val();
+
+      Meteor.call('sendEmail',
+                  'ehourany@gmail.com, labungan@gmail.com',
+                  email,
+                  '',
+                  'Message from ' + fullname,
+                  fullname + ' has submitted a message: \n' + message);
+
+      template.$('.thankyou').toggleClass('hide');
+      template.$('.form-horizontal').toggleClass('hide');
+
+      return false;
+    }
+  });
+
+  Template.rsvp.events({
+    "change #numberGuests": function (e, template) {
+      var numberGuests = +template.$('#numberGuests').val(),
+          guestForm = template.$('.guest-form'),
+          guestConstructor = $('<div class="form-horizontal" id="guestForm">');
+
+      if (numberGuests > 0 && numberGuests < 6) {
+        for (var i = 1; i <= numberGuests; i++) {
+          guestSelection = $('<select>').addClass('form-control guest-culinary').attr('id', 'guest'+i).append($('<option>Please select culinary option</option><option>Beef</option><option>Mahi Mahi</option><option>Vegetarian</option>'));
+          $(guestConstructor).append($('<div>').addClass('form-group col-md-12')
+                             .append($('<input type="text" class="form-control guest-form-input" placeholder="Name for Guest #' + i + '">').attr('id', 'guest' + i))
+                             .append(guestSelection));
+        };
+      } else if (numberGuests >= 6) {
+        $(guestConstructor).append($('<h4>').addClass('normal').text('Please specify names & culinary preference for each person below'))
+                           .append($('<textarea rows="4">').addClass('form-control').attr('id', 'largeParty'));
+      }
+      $(guestForm).html(guestConstructor);
+    },
+    "change #repondez input[name=optionsWedding]": function (e, template) {
+      var repondez = template.$('input[type=radio]:checked').val();
+      if (repondez === 'no') {
+        template.$('.response-yes, .response-no, #guestSelect').toggleClass('hide');
+      } else {
+        template.$('.response-yes, .response-no, #guestSelect').toggleClass('hide');
+      }
+    },
+    "submit .rsvp-submit": function (e, template) {
+      e.preventDefault();
+      e.stopPropagation();
+      var fullname = template.$('#fullname').val(),
+          email = template.$('#emailaddress').val(),
+          mealMain = template.$('#guestSelect').val(),
+          numberGuests = template.$('#numberGuests').val(),
+          guestNames = _(template.$('.guest-form-input:visible')).map(function (v) { return $(v).val(); }),
+          mealGuests = _(template.$('.guest-culinary')).map(function (v) { return $(v).val(); }),
+          attendance = template.$('input[type=radio]:checked').val(),
+          guestsAndMeals = _.flatten(_.zip(guestNames, mealGuests)),
+          thankyou = template.$('.thankyou'),
+          formattedNames = '',
+          formattedMeals = '';
+
+      _(guestNames).each(function (v, k) { formattedNames += v + (k === guestNames.length ? '' : ', ')});
+      _(mealGuests).each(function (v, k) { formattedMeals += v + (k === mealGuests.length ? '' : ', ')});
+
+      var isEmail = validator.isEmail(email),
+          isName = validator.isAlpha(fullname);
+
+      if (!(isEmail && isName)) {
+        if (fullname == '') {
+          template.$('.required:first').removeClass('hide');
+        } else {
+          template.$('.required:first').text('* Is that your real name?').removeClass('hide');
+        }
+        if (email == '') {
+          template.$('.required:last').removeClass('hide');
+        } else {
+          template.$('.required:last').text('* Really, that\'s your email address?').removeClass('hide');
+        }
+        if (isEmail) {
+          template.$('.required:last').addClass('hide');
+        }
+        if (isName) {
+          template.$('.required:first').addClass('hide');
+        }
+      } else {
+        template.$('.required:visible').addClass('hide');
+        Meteor.call('sendEmail',
+                    'ehourany@gmail.com, labungan@gmail.com',
+                    email,
+                    '',
+                    'RSVP from ' + fullname,
+                    'Hello!\nAn invitee has RSVP\'d. Response below.\nName: ' + fullname
+                        + '\nEmail: ' + email 
+                        + '\nAttending? ' + attendance
+                        + '\nNumber of Guests: ' + numberGuests
+                        + '\nGuest Names: ' + formattedNames
+                        + '\nMeal Preferences (in order): ' + formattedMeals);
+
+        $(thankyou).toggleClass('hide');
+        $('.bottom-response, .left-column, .right-column').addClass('hide');
+      }      
+      return false;
+    }
+  });
+
+  Template.slideshow.rendered = function () {
+      $('.slideshow').slick({
+        dots: false,
+        infinite: true,
+        speed: 300,
+        fade: true,
+        draggable: true,
+        cssEase: 'linear'
+    });
+  }
+
+  Template.groomsmen.rendered = function () {
+    $('.groomsmen').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        fade: true,
+        draggable: true,
+        cssEase: 'linear',
+        centerMode: true,
+        mobileFirst: true,
+        variableWidth: false,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              centerMode: true
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              centerMode: true
+            }
+          }
+        ]
+    });
+  }
+
+  Template.bridesmaids.rendered = function () {
+    $('.bridesmaids').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        fade: true,
+        draggable: true,
+        cssEase: 'linear',
+        centerMode: true,
+        mobileFirst: true,
+        variableWidth: false,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              centerMode: true
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              centerMode: true
+            }
+          }
+        ]
+    });
+  }
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+    Meteor.methods({
+      sendEmail: function (to, from, cc, subject, text) {
+        check([to, from, cc, subject, text], [String]);
+
+        // Let other method calls from the same client start running,
+        // without waiting for the email sending to complete.
+        this.unblock();
+
+        Email.send({
+          to: to,
+          from: from,
+          cc: cc,
+          subject: subject,
+          text: text
+        });
+      }
+    });
+  });
+}
+
+Router.route('/', function () {
+  this.render('index');
+});
+
+Router.route('/info', function () {
+  this.render('info');
+});
+
+Router.route('/aboutus', function () {
+  this.render('aboutus');
+});
+
+Router.route('/weddingparty', function () {
+  this.render('weddingparty');
+});
+
+Router.route('/gallery', function () {
+  this.render('gallery');
+});
+
+Router.route('/registry', function () {
+  this.render('registry');
+});
+
+Router.route('/lodging', function () {
+  this.render('lodging');
+});
+
+Router.route('/contactus', function () {
+  this.render('contactus');
+});
+
+Router.route('/rsvp', function () {
+  this.render('rsvp');
+});
